e2e58f433859aa6b54e4109c0c7a7a220f62b7a5
Reko Jokelainen <reko.jokelainen@nitorcreations.com>
2014-05-19 08:39:31 +0300

    Add dist/

---
+++ dist/componitor.js
@@ -0,0 +1,131 @@
+/**
+ * @ngdoc overview
+ * @name componitor
+ * @module componitor
+ *
+ * @description
+ * Generate simple HTML template -like directives on the fly.
+ * Currently, the templates need the present on application bootstrap to be correctly loaded.
+ *
+ * @example
+<example module="componitor">
+ <file name="index.html">
+
+  <div class="clearfix" ng-controller="DemoCtrl as ctrl">
+    <h1>There should be a two boxes below with different contents</h1>
+    Title: <input type="text" ng-model="ctrl.title" />
+    Body: <input type="text" ng-model="ctrl.body" />
+
+
+    <my-box>
+      <my-box-title>{{ctrl.title}}</my-box-title>
+      <my-box-body>{{ctrl.body}}</my-box-body>
+    </my-box>
+
+    <my-box>
+      <my-box-title>My title is: {{ctrl.title}}</my-box-title>
+      <my-box-body>And the body: {{ctrl.body}}</my-box-body>
+      <!-- This will be ignored -->
+      <my-box-body>This should never be shown</my-box-body>
+    </my-box>
+
+  </div>
+  <!-- Uses the same camelCase style as angularjs directives -->
+  <componitor-template name="myBox">
+    <!-- The template goes here -->
+    <div class="my-box">
+      <h3><content selector="my-box-title" /></h3>
+      <p><content selector="my-box-body" /></h3>
+    </div>
+  </componitor-template>
+
+ </file>
+ <file name="script.js">
+  function DemoCtrl() {
+    this.title = "Title";
+    this.body = "Lorem ipsum...";
+  }
+ </file>
+ <file name="style.css">
+  .my-box {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+    float: left;
+    width: 150px;
+    height: 150px;
+    border: 1px solid #333;
+    margin-left: 10px;
+  }
+ .clearfix:after {
+   content: ' ';
+   display: block;
+   clear: both;
+ }
+ </file>
+</example>
+ */
+/**
+ * @ngdoc directive
+ * @name componitor.directive:componitorTemplate
+ * @module componitor
+ * @restrict AE
+ *
+ * @param {String} name an AngularJs-style camelCased representation of the created directive's name. Use two part names containing a dash to avoid future problems.
+ *
+ * @description
+ * Registers a new html template directive with the given `name`. The componitor-template is
+ * automatically hidden from view by settings `style="display:none;"`.
+ *
+ * NOTE: Not a real directive. The contents of the `<componitor-template />` elements are parsed in the `.config`
+ * method. I.e., the tags must be present when the angular application is bootstrapped.
+ *
+ *
+ * For usage example see {@link componitor}
+ *
+ */
+var componitor = angular.module('componitor', [])
+  .config(['$compileProvider', function($compileProvider) {
+    function createDirective(name, templateHtml) {
+
+      $compileProvider.directive(name, ['$compile', function($compile) {
+        return {
+          scope: true,
+          restrict: 'E',
+          terminal: true,
+          link: function(s,realElem) {
+            realElem.addClass('componitor-component');
+            realElem.addClass('componitor-component-' + name);
+
+            var template = angular.element('<p>').append(angular.element(templateHtml).clone());
+            // Find the content elements to be replaced by their selectors
+            var contents = template.find('content');
+            angular.forEach(contents, function(c) {
+              // Replace each of the <content /> elements with their selector
+              var contentTag = angular.element(c);
+              var selector = contentTag.attr('selector');
+              var realContent = realElem.find(selector).html();
+
+              contentTag.replaceWith(realContent);
+            });
+
+            // Replace with the populated template and $compile
+            realElem.html(template.html());
+            $compile(realElem.contents())(s);
+          }
+        };
+      }]);
+    }
+
+    var templates = angular.element(document.body).find('componitor-template');
+    angular.forEach(templates, function(t) {
+      var templateElement = angular.element(t);
+      templateElement.css({display: 'none'});
+      var name = templateElement.attr('name');
+      if (!name) {
+        throw '<componitor-template /> name should be the camelCased name of the created directive';
+      }
+      createDirective(name, templateElement.html());
+    });
+
+  }]);
---
+++ dist/docs/js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "componitor",
+      "shortName": "componitor",
+      "type": "overview",
+      "moduleName": "componitor",
+      "shortDescription": "Generate simple HTML template -like directives on the fly.",
+      "keywords": "angularjs api application block body bootstrap border boxes camelcase center class clear clearfix componitor content contents correctly css ctrl currently democtrl directives display float fly function generate height html ignored ipsum js left loaded lorem margin-left middle module my-box my-box-body my-box-title mybox ng-controller ng-model overview script selector simple solid style table-cell template templates text text-align title type vertical-align width"
+    },
+    {
+      "section": "api",
+      "id": "componitor.directive:componitorTemplate",
+      "shortName": "componitorTemplate",
+      "type": "directive",
+      "moduleName": "componitor",
+      "shortDescription": "Registers a new html template directive with the given name. The componitor-template is",
+      "keywords": "ae angular angularjs-style api application automatically avoid bootstrapped camelcased componitor componitor-template config contents created dash directive display elements example future hidden html method names note parsed problems real registers representation settings style tags template usage view"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "api/componitor",
+  "scripts": [
+    "angular.min.js",
+    "componitor.js"
+  ]
+};
